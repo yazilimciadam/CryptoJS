97ff7d73f30d7364a101083e60ddf2d8c193beaf
yazilimciadam <tasmehmet25@outlook.com>
2022-12-12 16:10:04 +0300

    mm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 80
-app.get('/', (req, res) => {
+app.get('/adana', (req, res) => {
  res.send('Hello World!')
 })
 app.listen(port, () => {
